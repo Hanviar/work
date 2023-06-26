1b1d79e26af44f3b4d0f106d658c819b01741e2a
Hanviar <137649831+Hanviar@users.noreply.github.com>
2023-06-26 22:01:12 +0300

    Update moremusic.sql

--- moremusic.sql
+++ moremusic.sql
@@ -14,14 +14,14 @@ insert into artist(artist_name) values
   ('Лоретта Линн'),
   ('Джими Хендрикс');
 insert into GenreArtist(genre_id, artist_id) values
-  ('1,1'),
-  ('1,2'),
-  ('2,3'),
-  ('4,4'),
-  ('2,5'),
-  ('2,6'),
-  ('3,7'),
-  ('5,8');
+  ('1', '1'),
+  ('1', '2'),
+  ('2', '3'),
+  ('4', '4'),
+  ('2', '5'),
+  ('2', '6'),
+  ('3', '7'),
+  ('5', '8');
 insert into album (album_name, year_of) values
   ('The Massacre', 1998),
   ('Encore', 2019),
@@ -32,14 +32,14 @@ insert into album (album_name, year_of) values
   ('Full sircle', 2020),
   ('Band of Gypsys', 2019);
 insert into artistalbum (artist_id, album_id) values
-  ('1,1'),
-  ('2,2'),
-  ('3,3'),
-  ('4,4'),
-  ('5,5'),
-  ('6,6'),
-  ('7,7'),
-  ('8,8');
+  ('1', '1'),
+  ('2', '2'),
+  ('3', '3'),
+  ('4', '4'),
+  ('5', '5'),
+  ('6', '6'),
+  ('7', '7'),
+  ('8', '8');
 insert into songs(songs_name, duration, album_id) values
   ('My Toy Soldier', 123, 1),
   ('Curtains Up', 642, 2),
@@ -66,18 +66,18 @@ insert into collection(collection_name, year_of) values
   ('Коллекционное издание 7', 2016),
   ('Коллекционное издание 8', 1970);
 insert into songscollection(songs_id, collection_id) values
-  ('1,1'),
-  ('2,1'),
-  ('3,2'),
-  ('4,3'),
-  ('5,4'),
-  ('6,4'),
-  ('7,5'),
-  ('8,5'),
-  ('9,6'),
-  ('10,6'),
-  ('11,7'),
-  ('12,7'),
-  ('13,8'),
-  ('14,8'),
-  ('15,8');
+  ('1', '1'),
+  ('2', '1'),
+  ('3', '2'),
+  ('4', '3'),
+  ('5', '4'),
+  ('6', '4'),
+  ('7', '5'),
+  ('8', '5'),
+  ('9', '6'),
+  ('10', '6'),
+  ('11', '7'),
+  ('12', '7'),
+  ('13', '8'),
+  ('14', '8'),
+  ('15', '8');

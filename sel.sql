07c086b5e18df0964168173da07394fe424c6fb3
Hanviar <137649831+Hanviar@users.noreply.github.com>
2023-06-26 17:08:15 +0300

    Update sel.sql

--- sel.sql
+++ sel.sql
@@ -1,9 +1,9 @@
 select album_name, year_of from album
 where year_of = 2018;
 
-select songs_name< duration from songs
+select songs_name, duration from songs
 order by duration desc
-limit = 1;
+limit 1;
 
 select song_name from songs
 where duration >= 3.5*60;

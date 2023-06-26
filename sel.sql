6149ca22dcb1dedd548e576c8de7c3ae21d3ca1c
Hanviar <137649831+Hanviar@users.noreply.github.com>
2023-06-26 14:28:22 +0300

    Create sel.sql

---
+++ sel.sql
@@ -0,0 +1,18 @@
+select album_name, year_of from album
+where year_of = 2018;
+
+select songs_name< duration from songs
+order by duration desc
+limit = 1;
+
+select song_name from songs
+where duration >= 3.5*60;
+
+select collection_name, year_of from collection
+where year_of between 2018 and 2021;
+
+select artist_name from artist
+where artist_name not like '% %';
+
+select song_name from songs
+where lower(song_name) like '%мой%' or lower(song_name) like '%my%';
